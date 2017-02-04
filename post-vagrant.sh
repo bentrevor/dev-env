ab1b07d99670a21eeadd9f46fcc058a3557a62ed
Ben Trevor <benjamin.trevor@gmail.com>
2017-02-04 03:42:42 -0600

    fix ag

--- post-vagrant.sh
+++ post-vagrant.sh
@@ -6,5 +6,6 @@ source /home/vagrant/env/configure.sh
 
 sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
 git clone git@github.com:ggreer/the_silver_searcher.git
-source /home/vagrant/the_silver_searcher/build.sh
-sudo make install -C /home/vagrant/the_silver_searcher
+cd the_silver_searcher
+./build.sh
+sudo make install
