d8bd8c91a2bccb14f8331f526a3d669ff032e7df
Ben Trevor <benjamin.trevor@gmail.com>
2018-07-30 00:26:23 +0000

    updates

--- configure.sh
+++ configure.sh
@@ -6,18 +6,61 @@ function symlink() {
 
     ln -s $target_path $link_path
     chown -R vagrant:vagrant $link_path
+    echo "$link_path -> $target_path" 
 }
 
-symlink /home/vagrant/env/emacs /home/vagrant/.emacs.d
+function maybe_show_existing_config() {
+	[[ -e "$1" ]] && echo "$1 already exists"
+}
 
-symlink /home/vagrant/env/zsh /home/vagrant/.zsh
-symlink /home/vagrant/.zsh/zshrc /home/vagrant/.zshrc
-symlink /home/vagrant/.zsh/zshenv /home/vagrant/.zshenv
+function configs_already_present() {
+        [[ -e /home/vagrant/.emacs.d ]] ||
 
-symlink /home/vagrant/env/dotfiles/vimrc /home/vagrant/.vimrc
-symlink /home/vagrant/env/dotfiles/tmux.conf /home/vagrant/.tmux.conf
-symlink /home/vagrant/env/dotfiles/gitconfig /home/vagrant/.gitconfig
+                [[ -e /home/vagrant/.gemrc ]] ||
+                [[ -e /home/vagrant/.gitconfig ]] ||
+                [[ -e /home/vagrant/.rspec ]] ||
+                [[ -e /home/vagrant/.tmux.conf ]] ||
+                [[ -e /home/vagrant/.vimrc ]] ||
 
-# doesn't work :(
-# chsh -s $(which zsh) vagrant
-echo "still need to run 'chsh -s $(which zsh) vagrant'"
+                [[ -e /home/vagrant/.zsh ]] ||
+                [[ -e /home/vagrant/.zshrc ]] ||
+                [[ -e /home/vagrant/.zshenv ]]
+}
+
+function show_existing_configs() {
+	maybe_show_existing_config /home/vagrant/.emacs.d
+
+	maybe_show_existing_config /home/vagrant/.gemrc
+	maybe_show_existing_config /home/vagrant/.gitconfig
+	maybe_show_existing_config /home/vagrant/.rspec
+	maybe_show_existing_config /home/vagrant/.tmux.conf
+	maybe_show_existing_config /home/vagrant/.vimrc
+
+	maybe_show_existing_config /home/vagrant/.zsh
+	maybe_show_existing_config /home/vagrant/.zshrc
+	maybe_show_existing_config /home/vagrant/.zshenv
+}
+
+function main() {
+	if configs_already_present; then
+		show_existing_configs
+	else
+		symlink /home/vagrant/env/emacs /home/vagrant/.emacs.d
+
+		symlink /home/vagrant/env/dotfiles/gemrc /home/vagrant/.gemrc
+		symlink /home/vagrant/env/dotfiles/gitconfig /home/vagrant/.gitconfig
+		symlink /home/vagrant/env/dotfiles/rspec /home/vagrant/.rspec
+		symlink /home/vagrant/env/dotfiles/tmux.conf /home/vagrant/.tmux.conf
+		symlink /home/vagrant/env/dotfiles/vimrc /home/vagrant/.vimrc
+
+		symlink /home/vagrant/env/zsh /home/vagrant/.zsh
+		symlink /home/vagrant/.zsh/zshrc /home/vagrant/.zshrc
+		symlink /home/vagrant/.zsh/zshenv /home/vagrant/.zshenv
+
+		# doesn't work :(
+		# chsh -s $(which zsh) vagrant
+		echo "\nstill need to run 'chsh -s $(which zsh) vagrant'"
+	fi
+}
+
+main
